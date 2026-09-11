042550199944d311f51d885de91af8397bb07154
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2026-09-11 15:51:30 -0400

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        // O(n) time solution
+        // notice that at the middle of the list is when the numbers
+        // split -> use this to weave
+        // - to find the middle: use slow and fast pointers
+        // - when fast reaches the end, slow will be at the middle
+        // - IMPORTANT DSA TRICK: after k iterations:
+        //      - slow has moved k nodes
+        //      - fast has moved 2k nodes
+
+        ListNode first = head;
+        ListNode second = head; // the second half; starting from mid
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        second = slow;
+        ListNode current = second;
+        ListNode prev = null;
+
+        // reverse the second half
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next; 
+        } 
+
+        second = prev;
+
+        // weave into the first half
+        while (first != null && second != null) {
+            // when second becomes null, there's nothing else to
+            // weave in
+            ListNode nextFirst = first.next;
+            ListNode nextSecond = second.next;
+            first.next = second;
+            second.next = nextFirst;
+            second = nextSecond;
+            first = nextFirst;
+        }
+    }
+}
